5d5826284a1abb9307ca8ac068002dee9a9d25e6
= <=>
2023-06-07 13:39:45 -0700

    add fundraise and contact sections

---
+++ contact-form-handler.php
@@ -0,0 +1,22 @@
+<?php
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $email_from = "zihanwang68@gmail.com";
+
+    $email_subject = "New Message For DECA!";
+
+    $email_body = "User Name: $name.\n"."User Email: $visitor_email.\n"."User Message: $message.\n";
+
+    $to = "zihanwang68@gmail.com";
+
+    $headers = "From: $email_from \r\n";
+
+    $headers .= "Reply-To: $visitor_email \r\n";
+
+    mail($to,$email_subject,$email_body,$headers);
+
+    header("Location: index.html");
+
+?>
